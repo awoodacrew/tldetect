c47152329d323befb3740a4e9fc9d793bf0510ee
Alec Woo <awoo.dacrew@gmail.com>
2017-10-25 12:59:12 -0700

    Script to train model using simulation images.

---
+++ ssd_inception_v2_sim_train.sh
@@ -0,0 +1,7 @@
+PATH_TO_YOUR_PIPELINE_CONFIG="./models/ssd_inception_v2_coco_11_06_2017_sim/ssd_inception_v2_coco.config"
+PATH_TO_TRAIN_DIR="./models/ssd_inception_v2_coco_11_06_2017_sim/train"
+
+python object_detection/train.py \
+    --logtostderr \
+    --pipeline_config_path=${PATH_TO_YOUR_PIPELINE_CONFIG} \
+    --train_dir=${PATH_TO_TRAIN_DIR}
